4cb2e69109b4c1e33c68ecdaf7341ba1fc5d3f43
Lee JuHwan <seungjoon0324@gmail.com>
2024-06-14 14:04:34 +0900

    6.14

--- src/jungol9017.java
+++ src/jungol9017.java
@@ -1,5 +1,13 @@
-public class jungol9017 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class jungol9017 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double a = Double.parseDouble(br.readLine());
+        double b = Double.parseDouble(br.readLine());
+        System.out.printf("x = %.2f\n", a);
+        System.out.printf("y = %.2f", b);
     }
 }
